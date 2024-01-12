7cd6975e96e4bb1b32f0ef3ad62ca3c01baaebcf
Ria.x <ria.x@mail.ru>
2024-01-12 21:15:12 +0300

    zadacha 3

--- task1/Program.cs
+++ task1/Program.cs
@@ -2,13 +2,21 @@
 
 
 
-int value = 100;
-Console.WriteLine(value);
+// int value = 100;
+// Console.WriteLine(value);
 
 
-double D = 5.55;
-char C = '!';
-bool B = true;
-Console.WriteLine(D);
-Console.WriteLine(C);
-Console.WriteLine(B);
+// double D = 5.55;
+// char C = '!';
+// bool B = true;
+// Console.WriteLine(D);
+// Console.WriteLine(C);
+// Console.WriteLine(B);
+
+
+using System.Diagnostics.CodeAnalysis;
+
+int i1 = 5;
+int i2 =6;
+int Sum = i1 + i2;
+Console.WriteLine(Sum);
