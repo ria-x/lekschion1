146c29f02bad98abd4add4d3d14264e0b53c4eea
Ria.x <ria.x@mail.ru>
2024-01-12 23:55:25 +0300

    zadacha 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,20 +3,44 @@
 // int sum = a + b;
 // Console.WriteLine(sum);
 
-int[] arr = new int[5];
-arr[0] = 5;
-arr[1] = 8;
-arr[2] = 3;
-arr[3] = 4;
-arr[4] = 1;
-Console.WriteLine(arr[0]);
-Console.WriteLine(arr[1]);
-Console.WriteLine(arr[2]);
-Console.WriteLine(arr[3]);
-Console.WriteLine(arr[4]);
+// int[] arr = new int[5];
+// arr[0] = 5;
+// arr[1] = 8;
+// arr[2] = 3;
+// arr[3] = 4;
+// arr[4] = 1;
+// Console.WriteLine(arr[0]);
+// Console.WriteLine(arr[1]);
+// Console.WriteLine(arr[2]);
+// Console.WriteLine(arr[3]);
+// Console.WriteLine(arr[4]);
 
 
-int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+// int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
 
-int[] arr3 = { 1, 2, 3, 4, 5 };
+// int[] arr3 = { 1, 2, 3, 4, 5 };
 
+
+// int n = 10;
+// int[] arr = new int[n];
+// int i = 0;
+// while (i < arr.Length)
+// {
+//     arr[i] = i + 1;
+//     // Console.Write(arr[i]);
+//     // Console.Write(' ');
+//     Console.Write($"{arr[i]} ");
+//     i = i + 1;
+// }
+
+int n = 10;
+int[] array = { 6, 4, 7, -5, 3, 4, 9, 3, 2.- 6 };
+int i = 0;
+while (i < n)
+{
+    if(array[i] % 2 == 0)
+    {
+        Console.Write($"{{array[i]} ");
+    }
+    i = i + 1;
+}
