f312e7477368f059819b39039c477b6a7c871eba
Ria.x <ria.x@mail.ru>
2024-01-12 22:02:04 +0300

    zadacha 4

--- task1/Program.cs
+++ task1/Program.cs
@@ -16,25 +16,47 @@
 
 
 
-int i1 = 5;
-int i2 =6;
-int Sum = i1 + i2;
-Console.WriteLine(Sum);
+// int i1 = 5;
+// int i2 =6;
+// int Sum = i1 + i2;
+// Console.WriteLine(Sum);
 
 
-long l1 = 6;
-int l2 = 5;
-long sum = l1 +l2;
-Console.WriteLine(sum);
+// long l1 = 6;
+// int l2 = 5;
+// long sum = l1 +l2;
+// Console.WriteLine(sum);
 
-double d1 = 5.55;
-double sum2 = l1 + d1;
-Console.WriteLine(sum2);
+// double d1 = 5.55;
+// double sum2 = l1 + d1;
+// Console.WriteLine(sum2);
 
 
-int produck1 = i1 * i2;
-long produck2 = l1 * l2;
-double produck3 = d1 * i2;
-Console.WriteLine(produck1);
-Console.WriteLine(produck2);
-Console.WriteLine(produck3);
+// int produck1 = i1 * i2;
+// long produck2 = l1 * l2;
+// double produck3 = d1 * i2;
+// Console.WriteLine(produck1);
+// Console.WriteLine(produck2);
+// Console.WriteLine(produck3);
+
+
+// int i1 = 5;
+// // int i2 =6;
+// //int quotient = i1 / i2;
+// //Console.WriteLine(quotient);
+
+// double d1 = 6;
+// double quotient = i1 / d1;
+// Console.WriteLine(quotient);
+
+
+
+// int a =87;
+// int b =7;
+// if(a > b) {
+//     Console.WriteLine(a);
+// }
+// else {
+//     Console.WriteLine(b);
+// }
+
