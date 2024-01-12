6c0f815c2471df86791981098b001879c47165a3
Ria.x <ria.x@mail.ru>
2024-01-12 21:04:18 +0300

    zadacha 2

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello world");
+﻿//Console.WriteLine("Hello world");
+
+
+
+int value = 100;
+Console.WriteLine(value);
+
+
+double D = 5.55;
+char C = '!';
+bool B = true;
+Console.WriteLine(D);
+Console.WriteLine(C);
+Console.WriteLine(B);
