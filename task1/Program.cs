2a947f431a221ad73c9560039dfee4ce22eb362e
Ria.x <ria.x@mail.ru>
2024-01-12 21:29:14 +0300

    zadacha 4

--- task1/Program.cs
+++ task1/Program.cs
@@ -14,9 +14,27 @@
 // Console.WriteLine(B);
 
 
-using System.Diagnostics.CodeAnalysis;
+
 
 int i1 = 5;
 int i2 =6;
 int Sum = i1 + i2;
 Console.WriteLine(Sum);
+
+
+long l1 = 6;
+int l2 = 5;
+long sum = l1 +l2;
+Console.WriteLine(sum);
+
+double d1 = 5.55;
+double sum2 = l1 + d1;
+Console.WriteLine(sum2);
+
+
+int produck1 = i1 * i2;
+long produck2 = l1 * l2;
+double produck3 = d1 * i2;
+Console.WriteLine(produck1);
+Console.WriteLine(produck2);
+Console.WriteLine(produck3);
