8b849f4c8994f735a1c8d714e6a26aad911f5b2f
Ria.x <ria.x@mail.ru>
2024-01-12 23:03:52 +0300

    zadacha 1

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// int a = 1;
+// int b = 2;
+// int sum = a + b;
+// Console.WriteLine(sum);
+
+int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 8;
+arr[2] = 3;
+arr[3] = 4;
+arr[4] = 1;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+
+int[] arr3 = { 1, 2, 3, 4, 5 };
+
